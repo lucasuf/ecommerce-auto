4c9623d862f12901b0e1062b605f7d3ed8fc41b1
Lucas Barros <lucasbarros689@gmail.com>
2019-11-16 23:07:27 -0400

    Making presence of fields required

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+    validates :reference, presence :true
+    validates :purchase_channel, presence :true
+    validates :client_name, presence :true
+    validates :adress, presence :true
+    validates :delivery_service, presence :true
+    validates :total_value, presence :true
+    validates :line_items, presence :true
 end
