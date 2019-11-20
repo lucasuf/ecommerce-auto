3ff22e2618e3736c4c95d0bc1e4636c93b504746
Lucas Barros <lucasbarros689@gmail.com>
2019-11-20 18:19:05 -0400

    Creting new actions for update order status based on reference batch

--- app/controllers/api/v1/batches_controller.rb
+++ app/controllers/api/v1/batches_controller.rb
@@ -3,6 +3,13 @@
 We need a way to create a Batch and pass those Orders to the production status. 
 We should input the Purchase Channel and receive back the Reference of the created Batch 
 along with the number of Orders that were included.
+
+2. Produce a Batch
+Pass a Batch from production to closing using reference number.
+
+3. Send a Batch
+We also need a way to input a Batch and a Delivery Service and those Orders should be marked as sent.
+
 =end
 module Api
     module V1
@@ -23,14 +30,43 @@ module Api
                     orders.update(batch_id: batch[:id])
                     # Changing orders status
                     orders.update(status: Order.statuses['production'])
-					render json: {status: 'SUCCESS', message: 'Saved order', number_of_orders_in_batch: orders.size, data: batch}, status: :ok
+					render json: {status: 'SUCCESS', message: 'Saved Batch', number_of_orders_in_batch: orders.size, data: batch}, status: :ok
 				else
-					render json: {status: 'ERROR', message: 'Bat not saved', data:batch.errors}, status: :unprocessable_entity
+					render json: {status: 'ERROR', message: 'Batch not saved', data:batch.errors}, status: :unprocessable_entity
 				end
             end
+
+            def update
+                batch = Batch.where("reference_batch = ?", params[:reference_batch])
+                if params[:action_batch] == "Close"
+                    # Find all orders for this batch id
+                    orders = Order.where("batch_id = ?", batch[0].id)
+                    # Update status of orders to closing
+                    orders.update(status: Order.statuses['closing'])
+                    render json: {status: 'SUCCESS', message: 'Orders closed for batch', number_of_orders_in_batch: orders.size, data: batch}, status: :ok
+                elsif params[:action_batch] == "Send"
+                    if params[:delivery_service]
+                        # Find all orders for this batch id and delivery service
+                        orders = Order.where("batch_id = ? AND delivery_service = ?", batch[0].id, params[:delivery_service])
+                        # Update status of orders to sent
+                        orders.update(status: Order.statuses['sent'])
+                        render json: {status: 'SUCCESS', message: 'Orders sent for batch', delivery_service_sent: params[:delivery_service], number_of_orders_in_sent: orders.size, data: batch}, status: :ok
+                    else
+                        render json: {status: 'ERROR', message: 'Please provide a delivery service!!'}
+                    end
+                else
+                    render json: {status: 'ERROR', message: 'I do not know this action!!'}
+                end
+            end
+
             private
             def batch_params
                 params.permit(:purchase_channel_batch)
+            end
+
+            private
+            def update_params
+                params.permit(:action_batch, :delivery_service )
             end
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	namespace 'api' do
 		namespace 'v1' do
       resources :orders
-      resources :batches
+      resources :batches, param: :reference_batch
 		end
 	end
 end
